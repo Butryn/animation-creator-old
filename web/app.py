2b70eac8b501714ad5d1f8c9f2f564c71b38fd49
Butryn <r.butryn@op.pl>
2018-04-26 15:35:19 +0000

    Dzialajacy button

---
+++ web/app.py
@@ -0,0 +1,27 @@
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template(
+      'upload_files.html'
+)
+@app.route("/upload", methods='POST')
+def handle_upload():
+  if request.method == 'POST':
+    f = request.files['file']
+    f.save(secure_filename(f.filename))
+    return 'file uploaded successfully'
+
+@app.route("/make-animation")
+def make_animation():
+  return render_template(
+    "make_animation.html",
+    invitation="only limit is yourself"
+  )
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", port=8080, debug=True)
